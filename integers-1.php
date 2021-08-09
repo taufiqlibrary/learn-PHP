9077166e0664293963dafd065cff04d16c22e3ef
taufiqlibrary <taufiq.library@gmail.com>
2021-08-09 14:33:33 +0700

    Learn PHP
    Learn PHP

---
+++ integers-1.php
@@ -0,0 +1,7 @@
+<?php
+$a = 1234; // decimal number
+$a = 0123; // octal number (equivalent to 83 decimal)
+$a = 0x1A; // hexadecimal number (equivalent to 26 decimal)
+$a = 0b11111111; // binary number (equivalent to 255 decimal)
+$a = 1_234_567; // decimal number (as of PHP 7.4.0)
+?>
