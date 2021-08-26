e4d9f775e32556a00bd5e43ea48e4860a1e3a454
taufiqlibrary <taufiq.library@gmail.com>
2021-08-26 22:50:35 +0700

    Learn-PHP
    Learn-PHP

---
+++ Operator_Increment_dan_Decrement.php
@@ -0,0 +1,30 @@
+<?php 
+/**
+* Increment
+*/
+$a = 1;
+echo "a = {$a} <br />";
+$a++;
+echo "a = {$a} <br />";
+$a++;
+echo "a = {$a} <br />";
+
+# isi dari variabel $a dimasukkan lebih dulu ke variabel $b
+# lalu variabel $a menambah dirinya sendiri dengan angka 1
+$b = $a++;
+echo "b = {$b} <br />";
+echo "a = {$a} <br />";
+
+/**
+* Decrement
+*/
+$a--;
+echo "a = {$a} <br />";
+$a--;
+echo "a = {$a} <br />";
+
+# isi dari variabel $a dimasukkan lebih dulu ke variabel $b
+# lalu variabel $a mengurangi dirinya sendiri dengan angka 1
+$b = $a--;
+echo "b = {$b} <br />";
+echo "a = {$a} <br />";
