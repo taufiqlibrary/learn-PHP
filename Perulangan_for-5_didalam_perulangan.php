ed1177f19ea916bcfe21a76590ef072750f0d302
taufiqlibrary <taufiq.library@gmail.com>
2021-08-27 10:54:54 +0700

    Learn-PHP
    Learn-PHP

---
+++ Perulangan_for-5_didalam_perulangan.php
@@ -0,0 +1,11 @@
+<?php 
+
+# kode program ini akan menampilkan * dalam bentuk persegi
+# dengan ukuran 5x5
+for ($i = 0; $i < 5; $i++) {
+	for ($j = 0; $j < 5; $j++) {
+		echo "* ";
+	}
+
+	echo "<br />";
+}
