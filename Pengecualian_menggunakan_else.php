54e3cec14f5d8eeb958eecb34d4cf6cf634b3f01
taufiqlibrary <taufiq.library@gmail.com>
2021-08-27 09:29:15 +0700

    Learn-PHP
    Learn-PHP

---
+++ Pengecualian_menggunakan_else.php
@@ -0,0 +1,14 @@
+<?php 
+
+$nilai = 60;
+
+echo 'Memeriksa variable $nilai <br />';
+echo "Nilai: ($nilai) <br />";
+
+if ($nilai >= 70) {
+	echo "Selamat, siswa !";
+} else {
+	echo "Mohon maaf, siswa tidak lulus";
+}
+
+echo "<br />";
